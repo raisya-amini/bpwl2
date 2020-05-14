dc123a70de4c7098ce73c1f277ee967d64156792
raisya-amini <65360867+raisya-amini@users.noreply.github.com>
2020-05-15 00:31:35 +0700

    Update index.php

--- char/index.php
+++ char/index.php
@@ -87,7 +87,7 @@ var theme = {
 // tui.chart.registerTheme('myTheme', theme);
 // options.theme = 'myTheme';
 
-tui.chart.lineChart(container, data, options);
+tui.chart.barChart(container, data, options);
 </script>
 
 <!--For tutorial page-->
